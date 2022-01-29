b99969e57979c3f61f042d0554643150c22fec87
leeseungmin4966 <bae8416o@naver.com>
2022-01-29 17:11:13 +0900

    Feat : 배포 확인 메세지 루트주소에  return

--- src/main/java/com/example/nanuri/NanuriApplication.java
+++ src/main/java/com/example/nanuri/NanuriApplication.java
@@ -2,9 +2,19 @@ package com.example.nanuri;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class NanuriApplication {
+
+    @GetMapping("/")
+    public String home( ) {
+        return "nanuri server is running ...";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(NanuriApplication.class, args);
