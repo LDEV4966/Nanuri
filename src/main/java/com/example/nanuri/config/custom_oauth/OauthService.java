30703a57002549e3d2ea6d1d452f040907be8c40
leeseungmin4966 <bae8416o@naver.com>
2022-02-09 22:11:12 +0900

    Feat : Oauth 로그인 방식 변경
    기존의 코드를 발급 받아 진행하던 방식을 엑세스토큰을 받는것 부터 진행하는 걸로 수정함.

--- src/main/java/com/example/nanuri/config/custom_oauth/OauthService.java
+++ src/main/java/com/example/nanuri/config/custom_oauth/OauthService.java
@@ -9,14 +9,10 @@ import com.example.nanuri.config.jwt.JwtTokenProvider;
 import com.example.nanuri.service.token.RefreshTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
+
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Collections;
 import java.util.Map;
 
 @Service
@@ -28,20 +24,12 @@ public class OauthService {
     private final JwtTokenProvider jwtTokenProvider;
     private final RefreshTokenService refreshTokenService;
 
-    public LoginResponse login(String providerName, String code){
+    public LoginResponse login(String providerName, String oAuthAccessToken){
 
         OauthProvider provider = customInMemoryProviderRepository.findByProviderName(providerName);
 
-        OauthTokenResponse tokenResponse;
-        // access token 가져오기
-        if(providerName.equals("kakao")){ // 카카오는 예외적으로 accesstoken 얻어오는 방식 이 다름
-            tokenResponse = getKakaoToken(code,provider);
-        }else { // 구글, 네이버
-            tokenResponse = getToken(code, provider);
-        }
-
         // access token 으로 유저정보 가져오기
-        UserProfile userProfile = getUserProfile(providerName,tokenResponse,provider);
+        UserProfile userProfile = getUserProfile(providerName,oAuthAccessToken,provider);
 
         //유저 DB에 저장
         User user = saveOrUpdate(userProfile);
@@ -82,64 +70,20 @@ public class OauthService {
         return userRepository.save(user);
     }
 
-    private UserProfile getUserProfile(String providerName,OauthTokenResponse tokenResponse, OauthProvider provider ){
-        Map<String,Object> userAttributes = getUserAttributes(provider,tokenResponse);
+    private UserProfile getUserProfile(String providerName, String oAuthAccessToken, OauthProvider provider ){
+        Map<String,Object> userAttributes = getUserAttributes(provider,oAuthAccessToken);
         return OAuthAttributes.extract(providerName,userAttributes);
     }
-    private Map<String,Object> getUserAttributes(OauthProvider provider, OauthTokenResponse tokenResponse){
+
+    private Map<String,Object> getUserAttributes(OauthProvider provider, String oAuthAccessToken){
         return WebClient.create()
                 .get()
                 .uri(provider.getUserInfoUrl())
-                .headers(header -> header.setBearerAuth(tokenResponse.getAccessToken()))
+                .headers(header -> header.setBearerAuth(oAuthAccessToken))
                 .retrieve()
                 .bodyToMono(new ParameterizedTypeReference<Map<String,Object>>() {})
                 .block();
     }
 
-    private OauthTokenResponse getToken(String code, OauthProvider provider){
-        //webflux를 사용한 서버 통신
-        return WebClient.create()
-                .post()
-                .uri(provider.getTokenUrl())
-                .headers( header -> {
-                    header.setBasicAuth(provider.getClientId(),provider.getClientSecret());
-                    header.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-                    header.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-                    header.setAcceptCharset(Collections.singletonList(StandardCharsets.UTF_8));
-                })
-                .bodyValue(tokenRequest(code,provider))
-                .retrieve()
-                .bodyToMono(OauthTokenResponse.class)
-                .block();
-
-    }
-    private OauthTokenResponse getKakaoToken(String code, OauthProvider provider){
-        //webflux를 사용한 서버 통신
-        MultiValueMap<String,String> formData = new LinkedMultiValueMap<>();
-        formData.add("grant_type","authorization_code");
-        formData.add("client_id", provider.getClientId());
-        formData.add("redirect_uri",provider.getRedirectUrl());
-        formData.add("code",code);
-        formData.add("client_secret", provider.getClientSecret());
-        return WebClient.create()
-                .post()
-                .uri(provider.getTokenUrl())
-                .headers( header -> {
-                    header.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-                    header.setAcceptCharset(Collections.singletonList(StandardCharsets.UTF_8));
-                })
-                .bodyValue(formData)
-                .retrieve()
-                .bodyToMono(OauthTokenResponse.class)
-                .block();
-    }
-
-    private MultiValueMap<String,String> tokenRequest(String code, OauthProvider provider){
-        MultiValueMap<String,String> formData = new LinkedMultiValueMap<>();
-        formData.add("code",code);
-        formData.add("grant_type","authorization_code");
-        formData.add("redirect_uri",provider.getRedirectUrl());
-        return formData;
-    }
 
 }
--- src/main/java/com/example/nanuri/controller/test/OauthRestController.java
+++ src/main/java/com/example/nanuri/controller/test/OauthRestController.java
@@ -3,13 +3,9 @@ package com.example.nanuri.controller.test;
 import com.example.nanuri.config.custom_oauth.OauthService;
 import com.example.nanuri.dto.http.LoginResponse;
 
-import com.example.nanuri.config.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,11 +13,9 @@ public class OauthRestController {
 
     private final OauthService oauthService;
 
-    private final JwtTokenProvider jwtTokenProvider;
-
-    @GetMapping(path = "/login/oauth/{providerName}")
-    public ResponseEntity<LoginResponse> login(@PathVariable String providerName, @RequestParam String code){
-        LoginResponse loginResponse = oauthService.login(providerName,code);
+    @PostMapping(path = "/login/oauth/{providerName}")
+    public ResponseEntity<LoginResponse>  login(@PathVariable String providerName, @RequestParam String accessToken){
+        LoginResponse loginResponse = oauthService.login(providerName,accessToken);
         return ResponseEntity.ok().body(loginResponse);
     }
 }
