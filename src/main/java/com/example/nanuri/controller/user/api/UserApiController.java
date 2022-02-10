9f71c9c023813edbe22dfec06f89118f34ab7d44
leeseungmin4966 <bae8416o@naver.com>
2022-02-10 22:30:32 +0900

    Feat : 유저 정보 조회 및 유저가 생성한 레슨 리스트 목록 가져오기 API 생성 완료

---
+++ src/main/java/com/example/nanuri/controller/user/api/UserApiController.java
@@ -0,0 +1,31 @@
+package com.example.nanuri.controller.user.api;
+
+import com.example.nanuri.dto.http.BasicResponse;
+import com.example.nanuri.dto.http.SuccessDataResponse;
+import com.example.nanuri.dto.user.UserLessonResponseDto;
+import com.example.nanuri.dto.user.UserResponseDto;
+import com.example.nanuri.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserApiController {
+
+    private final UserService userService;
+
+    @GetMapping(path = "/user/info/{userId}")
+    public ResponseEntity<? extends BasicResponse> findById(@PathVariable Long userId){
+        UserResponseDto userResponseDto = userService.findById(userId);
+        return ResponseEntity.ok().body(new SuccessDataResponse<UserResponseDto>(userResponseDto));
+    }
+
+    @GetMapping(path = "/user/{userId}/lesson")
+    public ResponseEntity<? extends BasicResponse> findUserLessonById(@PathVariable Long userId){
+        UserLessonResponseDto userLessonResponseDto = userService.findUserLessonById(userId);
+        return ResponseEntity.ok().body(new SuccessDataResponse<UserLessonResponseDto>(userLessonResponseDto));
+    }
+}
